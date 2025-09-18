02d6e5608fb2e6a99eba57e7a489c16102b17ef6
NishraChavda <nishrachavda05@gmail.com>
2025-09-18 14:33:06 +0530

    Top.v

--- Code/Top.v
+++ Code/Top.v
@@ -1,10 +1,10 @@
-module ahb_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDTH=3)(
+module new_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDTH=3)(
 	input HRST,
 	input HCLK,
 	input enable,
+	input [1:0]SEL,
 	input [DATA_WIDTH-1:0]HWRITE_i,
 	input[ADDR_WIDTH-1:0]HADDR_i,
-	input [$clog2(SLAVE_NUM)-1:0]SEL,
 	input [2:0]HSIZE_i,
 	input HWRITE_e_i,
     input [HBURST_WIDTH-1:0]HBURST_i,
@@ -17,23 +17,9 @@ module ahb_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDT
     wire HREADY;
     wire HRESP;
     wire HRESP1;
-    wire HRESP2;
-    wire HRESP3;
-    wire HRESP4;
+	 wire [1:0]HSEL;
     wire HREADYOUT1;
-    wire HREADYOUT2;
-    wire HREADYOUT3;
-    wire HREADYOUT4;
     wire [DATA_WIDTH-1:0]HRDATA1;
-    wire [DATA_WIDTH-1:0]HRDATA2;
-    wire [DATA_WIDTH-1:0]HRDATA3;
-    wire [DATA_WIDTH-1:0]HRDATA4;
-    wire [$clog2(SLAVE_NUM)-1:0]HSEL;
-    wire HSEL_1;
-    wire HSEL_2;
-    wire HSEL_3;
-    wire HSEL_4;
-    wire [$clog2(SLAVE_NUM)-1:0]Multiplexor_SEL;
     wire HREQ;
     wire [ADDR_WIDTH-1:0]HADDR_o;
     wire HWRITE_o;
@@ -63,7 +49,6 @@ module ahb_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDT
 
 	 HREADY,
 	 HREQ,
-	 HSEL,
 	 HADDR_o,
 	 HWDATA_o,
 	 HSIZE_o,
@@ -75,13 +60,6 @@ module ahb_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDT
      DATA_o
 
 	);
-
- decoder du2(HSEL,
-	HSEL_1,
-	HSEL_2,
-	HSEL_3,
-	HSEL_4,
-    Multiplexor_SEL);
 
  slave slave1( HCLK,
  HRST,
@@ -95,74 +73,11 @@ module ahb_amba #(parameter DATA_WIDTH=32,ADDR_WIDTH=32,SLAVE_NUM=4, HBURST_WIDT
  HMASTERLOCK,
  HREADY,
  HWDATA_o,
- HREADYOUT1,
- HRESP1,
- HRDATA1);
+ HREADYOUT,
+ HRESP,
+ HRDATA);
 
- slave slave2( HCLK,
- HRST,
- HSEL_2,
-  HADDR_o,
- HWRITE_o,
- HSIZE_o,
- HBURST,
- HPROT,
- HTRANS,
- HMASTERLOCK,
- HREADY,
- HWDATA_o,
- HREADYOUT2,
- HRESP2,
- HRDATA2);
-
- slave slave3( HCLK,
- HRST,
- HSEL_3,
-  HADDR_o,
- HWRITE_o,
- HSIZE_o,
- HBURST,
- HPROT,
- HTRANS,
- HMASTERLOCK,
- HREADY,
- HWDATA_o,
- HREADYOUT3,
- HRESP3,
- HRDATA3);
-
- slave slave4( HCLK,
- HRST,
- HSEL_4,
-  HADDR_o,
- HWRITE_o,
- HSIZE_o,
- HBURST,
- HPROT,
- HTRANS,
- HMASTERLOCK,
- HREADY,
- HWDATA_o,
- HREADYOUT4,
- HRESP4,
- HRDATA4);
-
- multiplexor dut3(HRDATA1,
-	HRDATA2,
-	HRDATA3,
-	HRDATA4,
-     HRESP1,
-	 HRESP2,
-	HRESP3,
-	HRESP4,
-	HREADYOUT1,
-	HREADYOUT2,
-	HREADYOUT3,
-	HREADYOUT4,
-	Multiplexor_SEL,
-	HRdata_i,
-	HREADYOUT_m,
-	HRESP
- 	);
 
 endmodule
+
+
